a45ad83135996f945fff6578bde71df71bf0a33c
Josh Hunt <josh@trtr.co>
2019-12-06 16:45:28 +0000

    type use definitions

--- src/views/Triumphs/index.tsx
+++ src/views/Triumphs/index.tsx
@@ -2,11 +2,19 @@ import React from "react";
 
 import s from "./styles.module.scss";
 import { useDefinitions } from "../../lib/definitions";
+import { DestinyPresentationNodeDefinition as IDestinyPresentationNodeDefinition } from "bungie-api-ts/destiny2/interfaces";
+
+type Defs = {
+  DestinyPresentationNodeDefinition: Record<
+    string,
+    IDestinyPresentationNodeDefinition
+  >;
+};
 
 const Triumphs = function() {
   const { DestinyPresentationNodeDefinition } = useDefinitions(
     "DestinyPresentationNodeDefinition"
-  );
+  ) as Defs;
 
   console.log(
     "DestinyPresentationNodeDefinition:",
