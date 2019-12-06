4c4fe52590fca5d44cc66f131ffd93ea99406128
Josh Hunt <josh@trtr.co>
2019-12-06 17:33:07 +0000

    Render nodes

--- src/views/Triumphs/index.tsx
+++ src/views/Triumphs/index.tsx
@@ -2,28 +2,60 @@ import React from "react";
 
 import s from "./styles.module.scss";
 import { useDefinitions } from "../../lib/definitions";
-import { DestinyPresentationNodeDefinition as IDestinyPresentationNodeDefinition } from "bungie-api-ts/destiny2/interfaces";
+import { DestinyPresentationNodeDefinition as DestinyPresentationNodeDefinitionItem } from "bungie-api-ts/destiny2/interfaces";
+
+type DestinyPresentationNodeDefinition = Record<
+  number,
+  DestinyPresentationNodeDefinitionItem | undefined
+>;
 
 type Defs = {
-  DestinyPresentationNodeDefinition: Record<
-    string,
-    IDestinyPresentationNodeDefinition
-  >;
+  DestinyPresentationNodeDefinition?: DestinyPresentationNodeDefinition;
+};
+
+const ROOT_TRIUMPH_NODE = 1024788583;
+
+const Node: React.FC<{
+  presentationNodeHash: number;
+  presentationNodeDefinitions: DestinyPresentationNodeDefinition;
+}> = ({ presentationNodeHash, presentationNodeDefinitions }) => {
+  const triumphNode =
+    presentationNodeDefinitions &&
+    presentationNodeDefinitions[presentationNodeHash];
+
+  return triumphNode ? (
+    <div className={s.node}>
+      <p className={s.nodeHeading}>{triumphNode.displayProperties.name}</p>
+
+      <div>
+        {triumphNode.children.presentationNodes.map(child => {
+          return (
+            <Node
+              presentationNodeHash={child.presentationNodeHash}
+              presentationNodeDefinitions={presentationNodeDefinitions}
+            />
+          );
+        })}
+      </div>
+    </div>
+  ) : null;
 };
 
 const Triumphs = function() {
-  const { DestinyPresentationNodeDefinition } = useDefinitions(
-    "DestinyPresentationNodeDefinition"
-  ) as Defs;
-
-  console.log(
-    "DestinyPresentationNodeDefinition:",
-    DestinyPresentationNodeDefinition
-  );
+  const {
+    DestinyPresentationNodeDefinition: presentationNodeDefinitions
+  } = useDefinitions("DestinyPresentationNodeDefinition") as Defs;
 
   return (
     <div className={s.root}>
       <h2>Triumphs</h2>
+
+      {presentationNodeDefinitions && (
+        <Node
+          presentationNodeHash={ROOT_TRIUMPH_NODE}
+          presentationNodeDefinitions={presentationNodeDefinitions}
+        />
+      )}
     </div>
   );
 };
